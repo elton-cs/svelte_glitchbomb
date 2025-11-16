c65511ca8eb28d01590e0614693f294a676f511f
elton-cs <govada.elton@gmail.com>
2025-11-15 18:24:39 -0600

    feat: use mobile layout for desktop sizing

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,22 @@
 <script lang="ts">
   import GameContainer from "./lib/game_container/GameContainer.svelte";
-  import GameDashboard from "./lib/components/GameDashboard.svelte";
+  // import GameDashboard from "./lib/components/GameDashboard.svelte";
 
   let devMode = $state(false);
 </script>
 
-<!-- Desktop: show GameDashboard, hide GameContainer -->
-<div class="hidden md:block">
-  <GameDashboard {devMode} />
-</div>
-
-<!-- Mobile: show GameContainer, hide GameDashboard -->
+<!-- Mobile: show GameContainer -->
 <div
   class="md:hidden flex flex-col p-2 gap-2 h-full w-full justify-between min-h-0 overflow-hidden"
 >
   <GameContainer />
+</div>
+
+<!-- Desktop: show GameContainer with empty side divs -->
+<div class="hidden md:flex md:flex-row md:h-full md:w-full">
+  <div class="flex-1"></div>
+  <div class="flex flex-col p-2 gap-2 justify-between min-h-0 overflow-hidden">
+    <GameContainer />
+  </div>
+  <div class="flex-1"></div>
 </div>
