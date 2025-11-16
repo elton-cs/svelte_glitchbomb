554b42d9e7c0090fcb70956225164dc6b16b5650
elton-cs <govada.elton@gmail.com>
2025-11-16 07:33:41 -0600

    feat: remove extra divs over gamecontainer

--- src/App.svelte
+++ src/App.svelte
@@ -5,24 +5,10 @@
   let devMode = $state(false);
 </script>
 
+<GameContainer />
+
 <style>
   :global(body) {
     touch-action: manipulation;
   }
 </style>
-
-<!-- Mobile: show GameContainer -->
-<div
-  class="md:hidden flex flex-col p-2 gap-2 h-full w-full justify-between min-h-0 overflow-hidden"
->
-  <GameContainer />
-</div>
-
-<!-- Desktop: show GameContainer with empty side divs -->
-<div class="hidden md:flex md:flex-row md:h-full md:w-full">
-  <div class="flex-1"></div>
-  <div class="flex flex-col p-2 gap-2 justify-between min-h-0 overflow-hidden">
-    <GameContainer />
-  </div>
-  <div class="flex-1"></div>
-</div>
